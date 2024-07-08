5a336ac6f971519cb8721591ff37641eebecfcb0
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-07-08 08:34:06 +0530

    treesitter

--- init.lua
+++ init.lua
@@ -23,6 +23,9 @@ local plugins = {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    'nvim-treesitter/nvim-treesitter', build = ":TSUpdate"
   }
 
 }
@@ -32,6 +35,14 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  highlight = { enable = true },
+  indent = { enable = true }
+})
+
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
