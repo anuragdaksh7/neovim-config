4764315b9a6e47c7f0d9dde81d3b91dd9f85bb16
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-07-08 08:19:54 +0530

    telescope

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-
+vim.g.mapleader = " "
 
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy.nvim"
@@ -19,12 +19,19 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
 
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  }
 
 }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
