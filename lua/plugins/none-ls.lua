ba1786070be557f983399e1633572113a41728d3
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-07-12 10:18:04 +0530

    .

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,6 +7,7 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.completion.spell,
+        null_ls.builtins.diagnostics.stylelint,
       }
     })
 
